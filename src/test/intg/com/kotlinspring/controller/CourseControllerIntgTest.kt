362b840a9853fbdf44c9e2e913b765282163fe87
Anton Vlasik <antonvlasik@coherentsolutions.com>
2024-09-23 17:43:30 +0200

    add get all courses test

--- src/test/intg/com/kotlinspring/controller/CourseControllerIntgTest.kt
+++ src/test/intg/com/kotlinspring/controller/CourseControllerIntgTest.kt
@@ -3,7 +3,11 @@ package com.kotlinspring.controller
 import com.kotlinspring.restfulapi.RestfulApiApplication
 import com.kotlinspring.restfulapi.controller.CourseController
 import com.kotlinspring.restfulapi.dto.CourseDTO
+import com.kotlinspring.restfulapi.repository.CourseRepository
+import com.kotlinspring.util.courseEntityList
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
@@ -17,6 +21,16 @@ import org.springframework.test.web.reactive.server.WebTestClient
 class CourseControllerIntgTest {
     @Autowired
     lateinit var webTestClient: WebTestClient
+
+    @Autowired
+    lateinit var courseRepository: CourseRepository
+
+    @BeforeEach
+    fun setup() {
+        courseRepository.deleteAll()
+        val courses = courseEntityList()
+        courseRepository.saveAll(courses)
+    }
 
     @Test
     fun addCourse() {
@@ -35,5 +49,19 @@ class CourseControllerIntgTest {
         Assertions.assertTrue {
             result!!.id != null
         }
+    }
+
+    @Test
+    fun retrieveAllCourses() {
+        val result = webTestClient
+            .get()
+            .uri("/v1/courses")
+            .exchange()
+            .expectStatus().isOk
+            .expectBodyList(CourseDTO::class.java)
+            .returnResult()
+            .responseBody
+
+        assertEquals(3, result!!.size)
     }
 }
