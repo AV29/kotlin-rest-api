9f330967d5da32d10ef1a3b6a911af8e691a8927
Anton Vlasik <antonvlasik@coherentsolutions.com>
2024-09-23 16:36:54 +0200

    create course controller and post

---
+++ src/test/intg/com/kotlinspring/controller/CourseControllerIntgTest.kt
@@ -0,0 +1,39 @@
+package com.kotlinspring.controller
+
+import com.kotlinspring.restfulapi.RestfulApiApplication
+import com.kotlinspring.restfulapi.controller.CourseController
+import com.kotlinspring.restfulapi.dto.CourseDTO
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.reactive.server.WebTestClient
+
+@SpringBootTest(classes = [RestfulApiApplication::class, CourseController::class], webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@AutoConfigureWebTestClient
+class CourseControllerIntgTest {
+    @Autowired
+    lateinit var webTestClient: WebTestClient
+
+    @Test
+    fun addCourse() {
+        val courseDTO = CourseDTO(null, "name1", "category1")
+
+        val result = webTestClient
+            .post()
+            .uri("/v1/courses")
+            .bodyValue(courseDTO)
+            .exchange()
+            .expectStatus().isCreated
+            .expectBody(CourseDTO::class.java)
+            .returnResult()
+            .responseBody
+
+        Assertions.assertTrue {
+            result!!.id != null
+        }
+    }
+}
