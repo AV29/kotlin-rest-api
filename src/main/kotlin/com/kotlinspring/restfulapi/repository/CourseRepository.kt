c448287b6d370066b394f253b9da21e82883e4b8
Anton Vlasik <antonvlasik@coherentsolutions.com>
2024-10-01 14:42:38 +0200

    testing db layer with data jpa test

--- src/main/kotlin/com/kotlinspring/restfulapi/repository/CourseRepository.kt
+++ src/main/kotlin/com/kotlinspring/restfulapi/repository/CourseRepository.kt
@@ -1,6 +1,12 @@
 package com.kotlinspring.restfulapi.repository
 
 import com.kotlinspring.restfulapi.entity.Course
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 
-interface CourseRepository: CrudRepository<Course, Int>
+interface CourseRepository: CrudRepository<Course, Int> {
+    fun findByNameContaining(courseName: String): List<Course>
+
+    @Query(value = "SELECT * FROM COURSES WHERE name like %?1%", nativeQuery = true)
+    fun findCoursesByName(courseName: String): List<Course>
+}
---
+++ src/test/intg/com/kotlinspring/repository/CourseRepositoryIntgTest.kt
@@ -0,0 +1,65 @@
+package com.kotlinspring.repository
+
+import com.kotlinspring.restfulapi.RestfulApiApplication
+import com.kotlinspring.restfulapi.repository.CourseRepository
+import com.kotlinspring.util.courseEntityList
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
+import java.util.stream.Stream
+
+@DataJpaTest
+@ContextConfiguration(classes = [RestfulApiApplication::class])
+@ActiveProfiles("test")
+class CourseRepositoryIntgTest {
+
+    @Autowired
+    lateinit var courseRepository: CourseRepository
+
+    @BeforeEach
+    fun setup() {
+        courseRepository.deleteAll()
+        val courses = courseEntityList()
+        courseRepository.saveAll(courses)
+    }
+
+    @Test
+    fun findByNameContaining() {
+        val courses = courseRepository.findByNameContaining("SpringBoot");
+
+        Assertions.assertEquals(2, courses.size);
+    }
+
+    @Test
+    fun findCoursesByName() {
+        val courses = courseRepository.findCoursesByName("SpringBoot");
+
+        Assertions.assertEquals(2, courses.size);
+    }
+
+    @ParameterizedTest
+    @MethodSource("courseAndSize")
+    fun findCoursesByName_approach2(name: String, expectedSize: Int) {
+        val courses = courseRepository.findCoursesByName(name);
+
+        Assertions.assertEquals(expectedSize, courses.size);
+    }
+
+    companion object {
+        @JvmStatic
+        fun courseAndSize(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.arguments("SpringBoot", 2),
+                Arguments.arguments("Wiremock", 1)
+            )
+        }
+    }
+}
